7346072c00c895f731756413951f39be707b7951
Carlos La Fuente <carlos.lafuente.dev@gmail.com>
2021-12-16 12:50:25 -0400

    Models and request body

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
+# Python
+from typing import Optional
+from datetime import datetime
+
+# Pydantinc
+from pydantic import BaseModel
+
+# FastAPI
 from fastapi import FastAPI
+from fastapi import Body
+
 
 app = FastAPI()
 
+# Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    birthday: datetime
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
+
+# Path Operations
 @app.get("/")
 def home():
-    return {
-        "data": "Hello World!"
-    }
+    return { "data": "Hello World!" }
+
+# Request and Response Body
+@app.post("/user")
+def create_user(person: Person = Body(...)):
+
+    return person
